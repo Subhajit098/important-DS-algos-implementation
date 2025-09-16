27caff4648168c8d68d922c5265aacf5df2ed3d7
Subhajit098 <subhajitdey2388@gmail.com>
2025-09-17 00:47:20 +0530

    made keyword changes for DSU in graphs

--- graph/DSU.cpp
+++ graph/DSU.cpp
@@ -1,27 +1,27 @@
 //  DSU by Path Compression
-bool findParent(int node,vector<int>& parent){
-    if(node==parent[node]){
+int findParent(int node,vector<int>& parents){
+    if(node==parents[node]){
         return node;
     }
 
-    parent[node]=findParent(parent[node],parent);
+    return parents[node]=findParent(parents[node],parents);
 }
 
 // DSU by rank
-void union_by_rank(int x,int y,vector<int>& parent,vector<int> &rank){
-    int x_parent=findParent(x,parent);
-    int y_parent=findParent(y,parent);
+void union_by_rank(int x,int y,vector<int>& parents,vector<int>& ranks){
+    int x_parent=findParent(x,parents);
+    int y_parent=findParent(y,parents);
 
     if(x_parent==y_parent) return ;
 
     // if one's rank is more attach the less ranked one to the higher one to reduce the branching effect
-    if(rank[x_parent]>rank[y_parent]){
-        parent[y_parent]=x_parent;
-    } else if(rank[x_parent]<rank[y_parent]){
-        parent[x_parent]=y_parent;
+    if(ranks[x_parent]>ranks[y_parent]){
+        parents[y_parent]=x_parent;
+    } else if(ranks[x_parent]<ranks[y_parent]){
+        parents[x_parent]=y_parent;
     } else {
-        parent[x_parent]=y_parent;  // if ranks are same for both make any one of them as parent and increase the parent's rank;
-        rank[y_parent]++;
+        parents[x_parent]=y_parent;  // if ranks are same for both make any one of them as parent and increase the parent's rank;
+        ranks[y_parent]++;
     }
 
     return ;
@@ -29,6 +29,6 @@ void union_by_rank(int x,int y,vector<int>& parent,vector<int> &rank){
 
 
 // DSU by size
-void union_by_size(int x,int y,vector<int>& parent,vector<int> &rank){
-    
+void union_by_size(int x,int y,vector<int>& parents,vector<int> &ranks){
+
 }
